b9157fb233eb336365bfa591c5314f08a54490b4
Ranit Raj Ganguly <ranitrajganguly@gmail.com>
2023-10-31 04:15:00 -0600

    Adding @Module DI class and providing UserConfigManager and useCases

---
+++ app/src/main/java/com/example/instalens/di/AppModule.kt
@@ -0,0 +1,34 @@
+package com.example.instalens.di
+
+import android.app.Application
+import com.example.instalens.data.manager.LocalUserConfigManagerImpl
+import com.example.instalens.domain.manager.LocalUserConfigManager
+import com.example.instalens.domain.usecases.userconfig.ReadUserConfig
+import com.example.instalens.domain.usecases.userconfig.UserConfigUseCases
+import com.example.instalens.domain.usecases.userconfig.WriteUserConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+    @Provides
+    @Singleton
+    fun provideLocalUserConfigManager(
+        application: Application
+    ): LocalUserConfigManager = LocalUserConfigManagerImpl(application)
+
+
+    // Use-Cases
+    @Provides
+    @Singleton
+    fun provideUserConfigUseCases(
+        localUserConfigManager: LocalUserConfigManager
+    ): UserConfigUseCases = UserConfigUseCases(
+        readUserConfig = ReadUserConfig(localUserConfigManager),
+        writeUserConfig = WriteUserConfig(localUserConfigManager)
+    )
+}
