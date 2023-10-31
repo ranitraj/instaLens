9ca73bf1de6cd82d8afa9fa123fe3de1317f1343
Ranit Raj Ganguly <ranitrajganguly@gmail.com>
2023-10-31 04:35:01 -0600

    Implementing OnBoardingViewModel in presentation layer and defining method to call userConfig write useCase

---
+++ app/src/main/java/com/example/instalens/presentation/onboarding/viemodel/OnBoardingViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.instalens.presentation.onboarding.viemodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.instalens.domain.usecases.userconfig.UserConfigUseCases
+import com.example.instalens.presentation.onboarding.OnBoardingEvent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class OnBoardingViewModel @Inject constructor(
+    private val userConfigUseCases: UserConfigUseCases
+): ViewModel() {
+
+    fun onEvent(event: OnBoardingEvent) {
+        when(event) {
+            is OnBoardingEvent.WriteUserConfigToDataStore -> {
+                writeUserConfig()
+            }
+        }
+    }
+
+    /**
+     * Calls WriteUserConfig Use-Case to save the User-Config flag into UserPreference Datastore
+     */
+    private fun writeUserConfig() {
+        viewModelScope.launch {
+            userConfigUseCases.writeUserConfig
+        }
+    }
+}
