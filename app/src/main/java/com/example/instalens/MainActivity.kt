33c6ccd1c5b0a0ea7da3bb041764c0811070d6cc
Ranit Raj Ganguly <ranitrajganguly@gmail.com>
2023-10-30 20:00:17 -0600

    Initializing Splash Screen API in onCreate of MainActivity

--- app/src/main/java/com/example/instalens/MainActivity.kt
+++ app/src/main/java/com/example/instalens/MainActivity.kt
@@ -10,11 +10,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.instalens.ui.theme.InstaLensTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Initializing Splash Screen API
+        installSplashScreen()
+
         setContent {
             InstaLensTheme {
                 // A surface container using the 'background' color from the theme
