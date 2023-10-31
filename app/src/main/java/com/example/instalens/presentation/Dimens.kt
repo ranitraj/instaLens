6fa2bf33ee9a8ee151a520bae396fdf9130e4012
Ranit Raj Ganguly <ranitrajganguly@gmail.com>
2023-10-30 20:35:48 -0600

    Adding Dimens object class in presentation layer to create dimension constants

---
+++ app/src/main/java/com/example/instalens/presentation/Dimens.kt
@@ -0,0 +1,17 @@
+package com.example.instalens.presentation
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    // Padding
+    val Padding4dp = 4.dp
+    val Padding8dp = 8.dp
+    val Padding16dp = 16.dp
+    val Padding24dp = 24.dp
+
+    // Margin
+    val Margin4dp = 4.dp
+    val Margin8dp = 8.dp
+    val Margin16dp = 16.dp
+    val Margin24dp = 24.dp
+}
