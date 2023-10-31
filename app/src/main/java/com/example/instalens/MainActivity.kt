d6738c535bde100cb5c73fccac43713f1a6f5272
Ranit Raj Ganguly <ranitrajganguly@gmail.com>
2023-10-31 01:00:02 -0600

    Integrating OnBoardingScreen in MainActivity

--- app/src/main/java/com/example/instalens/MainActivity.kt
+++ app/src/main/java/com/example/instalens/MainActivity.kt
@@ -3,14 +3,12 @@ package com.example.instalens
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.instalens.presentation.onboarding.OnBoardingScreen
 import com.example.instalens.ui.theme.InstaLensTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,30 +20,10 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             InstaLensTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
+                    OnBoardingScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    InstaLensTheme {
-        Greeting("Android")
     }
 }
