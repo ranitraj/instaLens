3e3b6a4bac2f913ebc86b914e765d17f85355fbc
Ranit Raj Ganguly <ranitrajganguly@gmail.com>
2023-10-30 19:30:07 -0600

    Adding TfLite dependencies in app gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ android {
 }
 
 dependencies {
+    // Dependency Versions
+    val tfLiteVersion = "0.4.0"
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
@@ -59,11 +61,19 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    // Tensorflow-Lite Dependencies
+    implementation("org.tensorflow:tensorflow-lite-task-vision:$tfLiteVersion")
+    implementation("org.tensorflow:tensorflow-lite-gpu-delegate-plugin:$tfLiteVersion")
+    implementation("org.tensorflow:tensorflow-lite-gpu:2.9.0")
+
     testImplementation("junit:junit:4.13.2")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
