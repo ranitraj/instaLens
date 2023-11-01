ab6bc2b67e999596596b8a413a12dc751501d76d
Ranit Raj Ganguly <ranitrajganguly@gmail.com>
2023-11-01 00:37:23 -0600

    Fixing Issue: Slider not sliding onTouch

--- app/src/main/java/com/example/instalens/presentation/home/components/ThresholdLevelSlider.kt
+++ app/src/main/java/com/example/instalens/presentation/home/components/ThresholdLevelSlider.kt
@@ -4,11 +4,8 @@ import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
@@ -16,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
@@ -29,7 +27,7 @@ import com.example.instalens.utils.Constants.INITIAL_CONFIDENCE_SCORE
 fun ThresholdLevelSlider(thresholdValue: (Float) -> Unit) {
 
     // Initialize slider with threshold-level/ confidence-score of 0.5
-    val sliderValue by remember {
+    var sliderValue by remember {
         mutableFloatStateOf(INITIAL_CONFIDENCE_SCORE)
     }
 
@@ -48,6 +46,7 @@ fun ThresholdLevelSlider(thresholdValue: (Float) -> Unit) {
             Slider(
                 value = sliderValue,
                 onValueChange = {
+                    sliderValue = it
                     thresholdValue(it)
                 },
                 valueRange = 0f..1f,
