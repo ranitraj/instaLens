09500328737de25c57f86ee7b120cf40bc2fe5c4
Ranit Raj Ganguly <ranitrajganguly@gmail.com>
2023-11-02 10:39:28 -0600

    Adding Detection model class to obtain response from Model

---
+++ app/src/main/java/com/example/instalens/domain/model/Detection.kt
@@ -0,0 +1,16 @@
+package com.example.instalens.domain.model
+
+import android.graphics.RectF
+
+/**
+ * Represents a detected object in an image or frame.
+ *
+ * @param boundingBox The rectangle boundary of the detected object in the image.
+ * @param detectedObjectName The name or label of the detected object.
+ * @param confidenceScore The confidence score or probability (ranging between 0 and 1) indicating the accuracy of the detection.
+ */
+data class Detection(
+    val boundingBox: RectF,
+    val detectedObjectName: String,
+    val confidenceScore: Float
+)
